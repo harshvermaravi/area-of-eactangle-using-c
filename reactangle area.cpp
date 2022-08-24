717f52cbe5484a7236866e7140a23337d5ac53b0
Harsh Verma <99754649+harshvermaravi@users.noreply.github.com>
2022-08-24 19:27:34 +0530

    Add files via upload

---
+++ reactangle area.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int length,breadth;
+	printf("what is length of rectangle \n");
+	scanf("%d",&length);
+	printf("what is breadth of rectangle \n");
+	scanf("%d",&breadth);
+	printf("area of rectangle = %d",length*breadth);
+	return 0;
+}
+
